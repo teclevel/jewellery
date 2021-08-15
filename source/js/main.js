a24c95afec9f7260cd5f9f5e445a1558c99dfeec
Sergey <szaytsev060576@gmail.com>
2021-08-16 00:18:40 +0300

    заменяет 2 ф-ции на одну

--- source/js/main.js
+++ source/js/main.js
@@ -177,138 +177,199 @@
 
 /* overlay */
 
-function toggleOverlay(state, cb) {
-  const html = document.querySelector('html');
-  const marginSize = window.innerWidth - html.clientWidth;
+// function toggleOverlay(state, cb) {
+//   const html = document.querySelector('html');
+//   const marginSize = window.innerWidth - html.clientWidth;
 
-  if (state) {
-    html.style.marginRight = `${marginSize}px`;
+//   if (state) {
+//     html.style.marginRight = `${marginSize}px`;
 
-    document.querySelector('body').style.overflow = 'hidden';
+//     document.querySelector('body').style.overflow = 'hidden';
 
-    const overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    overlay.style.position = 'fixed';
-    overlay.style.top = '0';
-    overlay.style.left = '0';
-    overlay.style.width = '100%';
-    overlay.style.height = '100%';
-    overlay.style.zIndex = '20';
-    overlay.style.backgroundColor = 'black';
-    overlay.style.opacity = '.5';
-    document.body.append(overlay);
-    overlay.addEventListener('click', cb);
-  } else {
-    html.style.marginRight = '';
+//     const overlay = document.createElement('div');
+//     overlay.classList.add('overlay');
+//     overlay.style.position = 'fixed';
+//     overlay.style.top = '0';
+//     overlay.style.left = '0';
+//     overlay.style.width = '100%';
+//     overlay.style.height = '100%';
+//     overlay.style.zIndex = '20';
+//     overlay.style.backgroundColor = 'black';
+//     overlay.style.opacity = '.5';
+//     document.body.append(overlay);
+//     overlay.addEventListener('click', cb);
+//   } else {
+//     html.style.marginRight = '';
 
-    document.querySelector('body').style.overflow = '';
+//     document.querySelector('body').style.overflow = '';
 
-    const overlay = document.querySelector('.overlay');
-    overlay.remove();
-  }
-}
+//     const overlay = document.querySelector('.overlay');
+//     overlay.remove();
+//   }
+// }
 
 
 /* popup card */
 
-(function () {
+// (function () {
 
-  const card = document.querySelector('.modal-card');
-  if (!card) { return; }
+//   const card = document.querySelector('.modal-card');
+//   if (!card) { return; }
 
-  const cardToggle = document.querySelector('.card__add');
-  const buttonCardClose = card.querySelector('.modal-card__button-close');
-  const elementsPopup = Array.from(card.querySelectorAll('a, button, input'));
+//   const cardToggle = document.querySelector('.card__add');
+//   const buttonCardClose = card.querySelector('.modal-card__button-close');
+//   const elementsPopup = Array.from(card.querySelectorAll('a, button, input'));
 
-  cardToggle.addEventListener('click', onModalOpen);
-  buttonCardClose.addEventListener('click', onModalClose);
-  buttonCardClose.addEventListener('keydown', onModalButtonClose);
+//   cardToggle.addEventListener('click', onModalOpen);
+//   buttonCardClose.addEventListener('click', onModalClose);
+//   buttonCardClose.addEventListener('keydown', onModalButtonClose);
 
-  function onModalOpen(evt) {
-    evt.preventDefault();
+//   function onModalOpen(evt) {
+//     evt.preventDefault();
 
-    if (card) {
-      card.classList.remove('visually-hidden');
-    }
-    toggleOverlay(open, onModalClose);
+//     if (card) {
+//       card.classList.remove('visually-hidden');
+//     }
+//     toggleOverlay(open, onModalClose);
 
-    document.addEventListener('keydown', onModalKeydown);
-    elementsPopup[0].focus();
-  }
+//     document.addEventListener('keydown', onModalKeydown);
+//     elementsPopup[0].focus();
+//   }
 
-  function onModalClose() {
-    if (card) {
-      card.classList.add('visually-hidden');
-    }
-    toggleOverlay();
-  }
+//   function onModalClose() {
+//     if (card) {
+//       card.classList.add('visually-hidden');
+//     }
+//     toggleOverlay();
+//   }
 
-  function onModalButtonClose(evt) {
-    if (evt.code === 'Enter' || evt.code === 'Space') {
-      onModalClose();
-    }
-  }
+//   function onModalButtonClose(evt) {
+//     if (evt.code === 'Enter' || evt.code === 'Space') {
+//       onModalClose();
+//     }
+//   }
 
-  function onModalKeydown(evt) {
-    const focusedItemIndex = elementsPopup.indexOf(document.activeElement);
+//   function onModalKeydown(evt) {
+//     const focusedItemIndex = elementsPopup.indexOf(document.activeElement);
 
-    if (evt.shiftKey && evt.key === 'Tab' && focusedItemIndex === 0) {
-      elementsPopup[elementsPopup.length - 1].focus();
-      evt.preventDefault();
-    }
+//     if (evt.shiftKey && evt.key === 'Tab' && focusedItemIndex === 0) {
+//       elementsPopup[elementsPopup.length - 1].focus();
+//       evt.preventDefault();
+//     }
 
-    if (!evt.shiftKey && focusedItemIndex === elementsPopup.length - 1) {
-      elementsPopup[0].focus();
-      evt.preventDefault();
-    }
+//     if (!evt.shiftKey && focusedItemIndex === elementsPopup.length - 1) {
+//       elementsPopup[0].focus();
+//       evt.preventDefault();
+//     }
 
-    if (evt.code === 'Escape' || evt.code === 'Esc') {
-      onModalClose();
-    }
-  }
-})();
+//     if (evt.code === 'Escape' || evt.code === 'Esc') {
+//       onModalClose();
+//     }
+//   }
+// })();
 
 
 /* Popup login */
 
-(function () {
+// (function () {
 
-  const popup = document.querySelector('.modal');
-  const elementsPopup = Array.from(popup.querySelectorAll('input, button'));
+//   const popup = document.querySelector('.modal');
+//   const elementsPopup = Array.from(popup.querySelectorAll('input, button'));
 
-  const buttonClose = popup.querySelector('.modal__button-close');
-  const buttonsOpenPopup = document.querySelectorAll('.header__login');
+//   const buttonClose = popup.querySelector('.modal__button-close');
+//   const buttonsOpenPopup = document.querySelectorAll('.header__login');
 
-  buttonsOpenPopup.forEach((element) => {
+//   buttonsOpenPopup.forEach((element) => {
+//     element.addEventListener('click', onModalOpen);
+//   });
+
+//   buttonClose.addEventListener('click', onModalClose);
+//   buttonClose.addEventListener('keydown', onModalButtonClose);
+
+//   function onModalOpen(evt) {
+//     evt.preventDefault();
+//     if (popup) {
+//       popup.classList.remove('visually-hidden');
+//     }
+
+//     document.querySelector('body').style.overflow = 'hidden';
+//     toggleOverlay(open, onModalClose);
+
+//     document.addEventListener('keydown', onModalKeydown);
+
+//     elementsPopup[0].focus();
+//   }
+
+//   function onModalClose() {
+
+//     if (popup) {
+//       popup.classList.add('visually-hidden');
+//     }
+
+//     document.querySelector('body').style.overflow = '';
+//     toggleOverlay();
+//     document.removeEventListener('keydown', onModalKeydown);
+//   }
+
+//   function onModalButtonClose(evt) {
+//     if (evt.code === 'Enter' || evt.code === 'Space') {
+//       onModalClose();
+//     }
+//   }
+
+
+//   function onModalKeydown(evt) {
+//     const focusedItemIndex = elementsPopup.indexOf(document.activeElement);
+
+//     if (evt.shiftKey && evt.key === 'Tab' && focusedItemIndex === 0) {
+//       elementsPopup[elementsPopup.length - 1].focus();
+//       evt.preventDefault();
+//     }
+
+//     if (!evt.shiftKey && focusedItemIndex === elementsPopup.length - 1) {
+//       elementsPopup[0].focus();
+//       evt.preventDefault();
+//     }
+
+//     if (evt.code === 'Escape' || evt.code === 'Esc') {
+//       onModalClose();
+//     }
+//   }
+// })();
+
+/* пишем ф-цию поппап */
+
+
+function createPopup(classPopup, btnOpenPopup, btnClosePopup) {
+
+  const popup = document.querySelector(`.${classPopup}`);
+  if (!popup) { return; }
+
+  const popupSwitch = document.querySelectorAll(`.${btnOpenPopup}`);
+  const popupSwitchOff = popup.querySelector(`.${btnClosePopup}`);
+  const elementsPopup = Array.from(popup.querySelectorAll('a, button, input'));
+
+  popupSwitch.forEach((element) => {
     element.addEventListener('click', onModalOpen);
   });
 
-  buttonClose.addEventListener('click', onModalClose);
-  buttonClose.addEventListener('keydown', onModalButtonClose);
+  popupSwitch.addEventListener('click', onModalOpen);
+  popupSwitchOff.addEventListener('click', onModalClose);
+  popupSwitchOff.addEventListener('keydown', onModalButtonClose);
 
   function onModalOpen(evt) {
     evt.preventDefault();
-    if (popup) {
-      popup.classList.remove('visually-hidden');
-    }
 
-    document.querySelector('body').style.overflow = 'hidden';
+    popup.classList.remove('visually-hidden');
     toggleOverlay(open, onModalClose);
 
     document.addEventListener('keydown', onModalKeydown);
-
     elementsPopup[0].focus();
   }
 
   function onModalClose() {
-
-    if (popup) {
-      popup.classList.add('visually-hidden');
-    }
-
-    document.querySelector('body').style.overflow = '';
+    popup.classList.add('visually-hidden');
     toggleOverlay();
-    document.removeEventListener('keydown', onModalKeydown);
   }
 
   function onModalButtonClose(evt) {
@@ -316,7 +377,6 @@ function toggleOverlay(state, cb) {
       onModalClose();
     }
   }
-
 
   function onModalKeydown(evt) {
     const focusedItemIndex = elementsPopup.indexOf(document.activeElement);
@@ -335,4 +395,38 @@ function toggleOverlay(state, cb) {
       onModalClose();
     }
   }
-})();
+
+  function toggleOverlay(state, cb) {
+    const html = document.querySelector('html');
+    const marginSize = window.innerWidth - html.clientWidth;
+
+    if (state) {
+      html.style.marginRight = `${marginSize}px`;
+
+      document.querySelector('body').style.overflow = 'hidden';
+
+      const overlay = document.createElement('div');
+      overlay.classList.add('overlay');
+      overlay.style.position = 'fixed';
+      overlay.style.top = '0';
+      overlay.style.left = '0';
+      overlay.style.width = '100%';
+      overlay.style.height = '100%';
+      overlay.style.zIndex = '20';
+      overlay.style.backgroundColor = 'black';
+      overlay.style.opacity = '.5';
+      document.body.append(overlay);
+      overlay.addEventListener('click', cb);
+    } else {
+      html.style.marginRight = '';
+
+      document.querySelector('body').style.overflow = '';
+
+      const overlay = document.querySelector('.overlay');
+      overlay.remove();
+    }
+  }
+}
+
+createPopup('modal-card', 'card__add', 'modal-card__button-close');
+createPopup('modal', 'header__login', 'modal__button-close');
